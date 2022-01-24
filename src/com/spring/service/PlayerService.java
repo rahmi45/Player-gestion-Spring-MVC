c3a87f5d41f0fe0a44af304304f38f2c7fae4d57
RAHMI <rachid.rahmi@atos.net>
2022-01-24 22:48:17 +0100

    allPlayers service

--- src/com/spring/service/PlayerService.java
+++ src/com/spring/service/PlayerService.java
@@ -1,5 +1,11 @@
 package com.spring.service;
 
+import java.util.List;
+
+import com.spring.model.Player;
+
 public interface PlayerService {
 
+	public List<Player> allPlayer();
+		
 }
--- src/com/spring/service/PlayerServiceImpl.java
+++ src/com/spring/service/PlayerServiceImpl.java
@@ -1,8 +1,28 @@
 package com.spring.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.spring.dao.PlayerDAO;
+import com.spring.model.Player;
 
 @Service
 public class PlayerServiceImpl implements PlayerService{
+
+	private PlayerDAO playerDao;
+	
+	@Autowired
+	public PlayerServiceImpl(PlayerDAO playerDao) {
+		this.playerDao = playerDao;
+	}
+
+
+	@Override
+	public List<Player> allPlayer() {
+		// TODO Auto-generated method stub
+		return playerDao.getPlayers();
+	}
 
 }
