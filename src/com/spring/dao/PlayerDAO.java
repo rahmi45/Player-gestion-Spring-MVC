a80f36f9c2c274f70e38b06a76ae05d5d0eacd95
RAHMI <rachid.rahmi@atos.net>
2022-01-24 22:40:55 +0100

    getPlayers dao

--- src/com/spring/dao/PlayerDAO.java
+++ src/com/spring/dao/PlayerDAO.java
@@ -1,7 +1,11 @@
 package com.spring.dao;
 
+import java.util.List;
+
+import com.spring.model.Player;
+
 public interface PlayerDAO {
 	
-	
+	public List<Player> getPlayers();
 
 }
--- src/com/spring/dao/PlayerDAOImpl.java
+++ src/com/spring/dao/PlayerDAOImpl.java
@@ -1,8 +1,28 @@
 package com.spring.dao;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.spring.model.Player;
 
 @Repository
 public class PlayerDAOImpl implements PlayerDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public List<Player> getPlayers() {
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query<Player> query = session.createQuery("from Player", Player.class);
+		
+		return query.getResultList();
+	}
 
 }
