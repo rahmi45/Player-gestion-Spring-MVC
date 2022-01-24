65d32ab071940be70e1a4dd7da62296261d25558
RAHMI <rachid.rahmi@atos.net>
2022-01-24 23:01:00 +0100

    get all players controller

--- src/com/spring/controller/PlayerController.java
+++ src/com/spring/controller/PlayerController.java
@@ -1,8 +1,29 @@
 package com.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.model.Player;
+import com.spring.service.PlayerService;
 
 @Controller
+@RequestMapping("/fifa")
 public class PlayerController {
+	
+	private PlayerService playerService;
+	
+	@Autowired
+	public PlayerController(PlayerService playerService) {
+		this.playerService = playerService;
+	}
+	
+	@GetMapping("/players")
+	public List<Player> players(){
+		return playerService.allPlayer();
+	}
 
 }
