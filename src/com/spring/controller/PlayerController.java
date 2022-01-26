dfed18999ca70ca53498fad394117426996f861d
RAHMI <rachid.rahmi@atos.net>
2022-01-26 08:36:37 +0100

    get all Players handle controller

--- src/com/spring/controller/PlayerController.java
+++ src/com/spring/controller/PlayerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,8 +23,10 @@ public class PlayerController {
 	}
 	
 	@GetMapping("/players")
-	public List<Player> players(){
-		return playerService.allPlayer();
+	public String players(Model model){
+		List<Player> players = playerService.allPlayer();
+		model.addAttribute("players", players);
+		return "fifa-players";
 	}
 
 }
