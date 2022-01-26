4544c90ec5f85549db2c52a868e964620d2f4899
RAHMI <rachid.rahmi@atos.net>
2022-01-26 17:00:03 +0100

    GET & POST method

--- WebContent/WEB-INF/view/addPlayer.jsp
+++ WebContent/WEB-INF/view/addPlayer.jsp
@@ -10,7 +10,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<fo:form action="savePlayer"  modelAttribute="player"  method="GET" class="row g-3">
+<fo:form action="savePlayer"  modelAttribute="player"  class="row g-3">
   <div class="col-md-6">
     <label for="inputName" class="form-label">Name</label>
     <fo:input path="name" class="form-control" placeholder="Name"/>
--- src/com/spring/controller/PlayerController.java
+++ src/com/spring/controller/PlayerController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.model.Player;
@@ -39,7 +40,7 @@ public class PlayerController {
 	}
 	
 	//http://localhost:8088/Player/fifa/savePlayer
-	@GetMapping("/savePlayer")
+	@PostMapping("/savePlayer")
 	public String addPlayer(@ModelAttribute("player") Player Player) {
 		playerService.savePlayer(Player);
 		return "redirect:/fifa/players";
